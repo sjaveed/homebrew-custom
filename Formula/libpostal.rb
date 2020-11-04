2b9fafa3f00967721946b7b7170fa4935cc848cb
Shahbaz Javeed <sjaveed@gmail.com>
2020-11-03 19:44:01 -0500

    updated dependencies and data dir

--- Formula/libpostal.rb
+++ Formula/libpostal.rb
@@ -8,17 +8,22 @@ class Libpostal < Formula
   sha256 "c8a88eed70d8c09f68e1e69bcad35cb397e6ef11b3314e18a87b314c0a5b4e3a"
   license "MIT"
 
-  # depends_on "cmake" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
+    ENV["PATH"] = [ENV["PATH"], "/usr/local/bin"].join(':')
+
+    system "./bootstrap.sh"
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}",
-                          "--datadir=#{prefix}/var/db/libpostal"
-    # system "cmake", ".", *std_cmake_args
+                          "--datadir=#{var}/db/libpostal"
     system "make", "install"
   end
 
