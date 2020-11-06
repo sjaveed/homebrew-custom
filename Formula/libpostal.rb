cc7d97ccb38bdb4416f0667a5bcbfe3060dcb45c
Shahbaz Javeed <sjaveed@gmail.com>
2020-11-06 01:22:41 -0500

    removed generated comments, fixed paths, added test

--- Formula/libpostal.rb
+++ Formula/libpostal.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Libpostal < Formula
   desc "A C library for parsing/normalizing street addresses around the world. Powered by statistical NLP and open geo data."
   homepage "https://github.com/openvenues/libpostal"
@@ -14,8 +11,6 @@ class Libpostal < Formula
   depends_on "pkg-config" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
     ENV["PATH"] = [ENV["PATH"], "/usr/local/bin"].join(':')
 
     system "./bootstrap.sh"
@@ -23,20 +18,11 @@ class Libpostal < Formula
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}",
-                          "--datadir=#{var}/db/libpostal"
+                          "--datadir=#{var}/db"
     system "make", "install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test libpostal`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    File.exist?("#{prefix}/lib/libpostal.dylib")
   end
 end
